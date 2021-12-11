69ebff482455aed4fb855cfaa92798a10223a7f6
TA <tamilarasuva444@gmail.com>
2021-12-11 19:13:26 +0530

    12133212323

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+// dynamic year for footer
+document.getElementById("year").innerHTML=new Date().getFullYear();
+
+//text animator for homepage
 $("#js-rotating").Morphext({
 animation: "pulse",
 separator: ",",
 speed: 2000,
 complete: function() {}
 });
-
-document.getElementById("year").innerHTML=new Date().getFullYear();
